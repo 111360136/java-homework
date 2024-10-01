08b385aefe6985d9d9bdc2dcd66838e990fb9171
guan yi kai <t111360136@ntut.org.tw>
2024-10-01 14:34:07 +0800

    java homework

---
+++ homework/src/homework/p10.java
@@ -0,0 +1,12 @@
+package homework;
+
+public class p10 
+{
+	public static void main(String[] args) 
+	{
+		
+	System.out.println("歡迎使用java!");
+	System.out.println("開始使用java吧!");
+	
+	}
+}
---
+++ homework/src/homework/p16.java
@@ -0,0 +1,14 @@
+package homework;
+
+public class p16 
+{
+
+	public static void main(String[] args)
+	
+	{
+		System.out.println('A');
+		System.out.println("歡迎使用java");
+		System.out.println(123);
+	}
+
+}
